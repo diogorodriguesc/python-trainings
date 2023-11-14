6b87b7bf87a1d17a9b3893a341204a8f7e9c74f0
Diogo Correia <diogorodriguescorreia89@gmail.com>
2023-11-14 00:52:12 +0000

    Improved and fix for add-two-numbers leet-code-challenge.

--- leet-code-challenges/add-two-numbers.py
+++ leet-code-challenges/add-two-numbers.py
@@ -19,29 +19,26 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         transition = 0
-        total_list_node_root = ListNode()
-        total_list_node = total_list_node_root
+        total_list_node = total_list_node_root = ListNode()
 
         while(l1 or l2):
             l1_val = l1.val if l1 is not None else 0
             l2_val = l2.val if l2 is not None else 0
 
             total = l1_val + l2_val + transition
-
             transition = 1 if total > 9 else 0
+            total_list_node.val = total%10
 
-            total = total%10
-            total_list_node.val = total
-
-            if (l1 is not None):
+            if (l1):
                 l1 = l1.next
-            if (l2 is not None):    
+            if (l2):    
                 l2 = l2.next
 
-            if (l1 is not None or l2 is not None):
-                new_node = ListNode()
-                total_list_node.next = new_node
-                total_list_node = new_node
+            if (l1 or l2):
+                total_list_node.next = total_list_node = ListNode(transition)
+
+        if transition > 0:
+            total_list_node.next = total_list_node = ListNode(transition)
 
         return total_list_node_root
     
