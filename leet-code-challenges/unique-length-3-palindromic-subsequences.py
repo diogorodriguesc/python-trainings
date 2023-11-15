d44aa6cee07e7f3146be34ff7991711948b97cb6
Diogo Correia <diogorodriguescorreia89@gmail.com>
2023-11-15 08:26:15 +0000

    Applied few code improvements.

--- leet-code-challenges/unique-length-3-palindromic-subsequences.py
+++ leet-code-challenges/unique-length-3-palindromic-subsequences.py
@@ -4,34 +4,23 @@ import numpy
 
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        print("input %s" % (s))
-    
-        countLetters = dict()
         letterIndexes = dict()
         for index, letter in enumerate(s):
-            countLetters[letter] = (countLetters[letter] if countLetters.get(letter) else 0) + 1
             if not letterIndexes.get(letter):
                 letterIndexes[letter] = list()
                 letterIndexes[letter].append(index)
             else:
                 letterIndexes[letter].append(index)
 
-        print(letterIndexes)
-
         palindromics = list()
-        for indexX, letterA in enumerate(countLetters):
-            if countLetters[letterA] >= 3:
+        for letterA in letterIndexes.keys():
+            if len(letterIndexes[letterA]) >= 3:
                 palindromics.append("%s%s%s" % (letterA, letterA, letterA))
             
-            for indexY, letterB in enumerate(countLetters):
-                print(letterB)
-                print(letterIndexes[letterA][:1])
-                print(letterIndexes[letterB][:1])
-                print(letterIndexes[letterA][:1] > letterIndexes[letterB][:1])
-                if letterA is not letterB and countLetters[letterA] >=2 and countLetters[letterB] >= 1:
+            for letterB in letterIndexes.keys():
+                if letterA is not letterB and len(letterIndexes[letterA]) >=2 and len(letterIndexes[letterB]) >= 1:
                     palindromics.append("%s%s%s" % (letterA, letterB, letterA))
 
-        print(countLetters)
         print(palindromics)
 
         return len(numpy.unique(palindromics))
