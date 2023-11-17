dfa2647c60666666c21ceba6095269d854f92dd6
Diogo Correia <diogorodriguescorreia89@gmail.com>
2023-11-17 00:21:42 +0000

    Added improvement to find-unique-binary-string

--- challenges/find-unique-binary-string.py
+++ challenges/find-unique-binary-string.py
@@ -5,7 +5,7 @@ import numpy
 class Solution:
     def findDifferentBinaryString(self, nums: list[str]) -> str:
         bits = len(nums.__getitem__(0))
-        numbers_for_bits = range(1, self.__convert_binary_to_number__('1' * bits))
+        numbers_for_bits = range(0, self.__convert_binary_to_number__('1' * bits) + 1)
 
         numbers = list()
         for binary in map(str, nums):
@@ -25,11 +25,14 @@ class Solution:
                 missing_number = i
                 break
 
-        if missing_number is not None:
-            return self.__convert_number_to_binary__(missing_number)
+        # Could be a replacement for the above code, but, leetcode tells it doubles the runtime :O
+        # missing_number = set(numbers_for_bits).difference(numbers).pop()
 
-    def __convert_number_to_binary__(self, number) -> int:
-        return bin(number)[2:]
+        if missing_number is not None:
+            return self.__convert_number_to_binary__(missing_number).zfill(bits)
+
+    def __convert_number_to_binary__(self, number) -> str:
+        return bin(number).replace("0b", "")
     
     def __convert_binary_to_number__(self, binary) -> int:
         return int(binary, 2)
@@ -37,7 +40,9 @@ class Solution:
 solution = Solution()
 
 nums = list()
+nums.append('11')
+nums.append('01')
 nums.append('10')
-
+# nums.append('00')
 
 print(solution.findDifferentBinaryString(nums))
